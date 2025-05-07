3a6947530127968c00ed7b38bef48fd836cde90f
leall12 <arturalbertinileal@gmail.com>
2025-05-07 08:44:24 -0300

    Criando uma requisição Post

--- Projeto1/Controllers/HomeController.cs
+++ Projeto1/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Projeto1.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Sobre()
         {
             return View();
         }
--- Projeto1/Controllers/UsuarioController.cs
+++ Projeto1/Controllers/UsuarioController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projeto1.Repositorio;
 
 namespace Projeto1.Controllers
 {
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        /* DECLARANDO UMA VARIAVEL PRIVADA SOMENTE PARA LEITURA DO 
+         * TIPO UsuarioRepositorio CHAMADA _usuarioRepositorio */
+        private readonly UsuarioRepositorio _usuarioRepositorio; 
+
+        /* DEFININDO O CONSTRUTOR DA CLASSE UsuarioController QUE 
+         * VAI RECEBER UMA INSTÂNICA DE UsuarioReposiorio */
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio) 
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        } 
+        public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            return View();
+        }
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
     }
 }
